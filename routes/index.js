bcd4e155968c7b9f26da57de0f8746461d8de08f
Regpon <yuusuke@okamoto.us>
2018-08-24 23:17:42 +0900

    偉大なるレグポン

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '偉大なるレグポンの世界へようこそ' });
 });
 
 module.exports = router;
